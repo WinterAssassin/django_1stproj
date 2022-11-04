84ab099b1a4b6baeb56203d84baf77bc1853d0a7
MJ Cabrera <mjcabrera1996@gmail.com>
2022-11-04 08:47:22 +0800

    Test the added records in a web page

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -1,5 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Members
 # Create your views here.
 
 def index(request):
-    return render(request, 'members/myfirst.html')
+    mymembers = Members.objects.all().values()
+    # return render(request, 'members/myfirst.html')
+    output = ""
+    for x in mymembers:
+        output += x["firstname"]
+
+    return HttpResponse(output)

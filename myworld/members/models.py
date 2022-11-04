34648bb9bd4b6df0fd76b45df6dcb18019cd2c9e
MJ Cabrera <mjcabrera1996@gmail.com>
2022-11-04 08:02:44 +0800

    Create a venv and start of Django myworld project

---
+++ myworld/members/models.py
@@ -0,0 +1,8 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+class Members(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)

d6fcee6532e35e85e9331b4e8d4d3f6e7e605528
MJ Cabrera <mjcabrera1996@gmail.com>
2022-11-05 10:39:40 +0800

    Create addrecord view,addrecord temlate and update urls

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Members
 # Create your views here.
 
@@ -7,3 +7,13 @@ def index(request):
     mymembers = Members.objects.all().values()
     context = {'mymembers': mymembers}
     return render(request, 'members/index.html', context=context)
+
+def add(request):
+    return render(request, 'members/add.html')
+
+def addrecord(request):
+    x = request.POST['first']
+    y = request.POST['last']
+    member = Members(firstname=x, lastname=y)
+    member.save()
+    return redirect('index')
